55fa35db2b798dd13769d4c13d54c55759cd2da6
hjw9623 <hjw6191.wh@gmail.com>
2025-06-13 12:17:19 +0900

    feat: Poppins 폰트 추가 및 Typography 적용

--- app/src/main/java/com/hjw0623/pyeonking/ui/theme/Type.kt
+++ app/src/main/java/com/hjw0623/pyeonking/ui/theme/Type.kt
@@ -1,34 +1,85 @@
 package com.hjw0623.pyeonking.ui.theme
 
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.hjw0623.pyeonking.R
 
-// Set of Material typography styles to start with
+
+val Poppins = FontFamily(
+    Font(
+        resId = R.font.poppins_light,
+        weight = FontWeight.Light
+    ),
+    Font(
+        resId = R.font.poppins_regular,
+        weight = FontWeight.Normal
+    ),
+    Font(
+        resId = R.font.poppins_medium,
+        weight = FontWeight.Medium
+    ),
+    Font(
+        resId = R.font.poppins_semibold,
+        weight = FontWeight.SemiBold
+    ),
+    Font(
+        resId = R.font.poppins_bold,
+        weight = FontWeight.Bold
+    ),
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
+    titleMedium = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Bold,
+        color = DarkGreyBlack,
+        fontSize = 18.sp,
+        lineHeight = 23.sp,
         letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    titleLarge = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Bold,
+        color = DarkGreyBlack,
+        fontSize = 28.sp,
+        lineHeight = 22.sp,
+        letterSpacing = 0.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Bold,
+        color = DarkGreyBlack,
+        fontSize = 16.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        color = DarkGreyBlack,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        color = DarkGreyBlack,
+        fontSize = 14.sp,
+        lineHeight = 21.sp,
+        letterSpacing = 0.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        color = DarkGreyBlack,
+        fontSize = 12.sp,
+        lineHeight = 18.sp,
+        letterSpacing = 0.sp
     )
-    */
 )
