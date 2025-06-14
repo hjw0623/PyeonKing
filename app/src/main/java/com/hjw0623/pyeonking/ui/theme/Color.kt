1459a5bc13051d143b759538d042206d4539a9f2
hjw9623 <hjw6191.wh@gmail.com>
2025-06-15 05:45:21 +0900

    feat: 기존 색상 제거 및 색상 추가

--- app/src/main/java/com/hjw0623/pyeonking/ui/theme/Color.kt
+++ app/src/main/java/com/hjw0623/pyeonking/ui/theme/Color.kt
@@ -2,10 +2,18 @@ package com.hjw0623.pyeonking.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val BronzeOrange = Color(0xFFC87A2D)
+val DarkGreyBlack = Color(0xFF1C1C1C)
+val MiddleGrey = Color(0xFF7A7A7A)
+val LightGrey = Color(0xFFE0E0E0)
+val Ochre = Color(0xFF877863)
+val White = Color(0xFFFFFFFF)
+
+//brandColor
+val Cu = Color(0xFF673AB7)
+val Gs25 = Color(0xFF00897B)
+val Seven = Color(0xFF4CAF50)
+val Emart24 = Color(0xFFFF9800)
+
+val PyeonKingRed = Color(0xFFFF0000)
