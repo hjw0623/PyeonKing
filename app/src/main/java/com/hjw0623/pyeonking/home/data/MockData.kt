142124c97c93431529be8d60444c6f2d25e7ef73
hjw9623 <hjw6191.wh@gmail.com>
2025-06-22 21:28:13 +0900

    refactor: 홈 화면 관련 data class를 공통 모델로 리팩토링
    -RecommendItem -> Product로 통일

--- app/src/main/java/com/hjw0623/pyeonking/home/data/MockData.kt
+++ app/src/main/java/com/hjw0623/pyeonking/home/data/MockData.kt
@@ -1,37 +1,11 @@
 package com.hjw0623.pyeonking.home.data
 
+import com.hjw0623.pyeonking.core.data.mockProductList
 import com.hjw0623.pyeonking.home.presentation.HomeScreenState
-import java.util.UUID
-
-val mockRecommendItem1 = RecommendItem(
-    uuid = UUID.randomUUID().toString(),
-    imgUrl = "https://image.woodongs.com/imgsvr/item/GD_8801094083205_003.jpg",
-    name = "코카콜라제로캔350ML",
-    price = "2100",
-    priceForEach = "1050",
-    promotion = "1+1",
-    brand = "CU"
-)
-
-val mockRecommendItem2 = RecommendItem(
-    uuid = UUID.randomUUID().toString(),
-    imgUrl = "https://msave.emart24.co.kr/cmsbo/upload/nHq/plu_image/500x500/8809617945146.JPG",
-    name = "바프)카라멜솔티드아몬드앤프레첼40g",
-    price = "2500",
-    priceForEach = "1467",
-    promotion = "2+1",
-    brand = "EMART24"
-)
-val mockRecommendItemList = listOf(
-    mockRecommendItem1,
-    mockRecommendItem2,
-    mockRecommendItem1.copy(uuid = UUID.randomUUID().toString()),
-    mockRecommendItem1.copy(uuid = UUID.randomUUID().toString()),
-    mockRecommendItem1.copy(uuid = UUID.randomUUID().toString()),
-    mockRecommendItem1.copy(uuid = UUID.randomUUID().toString())
-)
 
 val mockHomeScreenState = HomeScreenState(
     isLoading = false,
-    recommendList = mockRecommendItemList
+    searchQuery = "",
+    selectedItemName = "",
+    recommendList = mockProductList
 )
--- app/src/main/java/com/hjw0623/pyeonking/home/presentation/HomeScreenState.kt
+++ app/src/main/java/com/hjw0623/pyeonking/home/presentation/HomeScreenState.kt
@@ -1,11 +1,11 @@
 package com.hjw0623.pyeonking.home.presentation
 
-import com.hjw0623.pyeonking.home.data.RecommendItem
-import com.hjw0623.pyeonking.home.data.mockRecommendItemList
+import com.hjw0623.pyeonking.core.data.Product
+
 
 data class HomeScreenState(
     val isLoading: Boolean = false,
     val searchQuery: String = "",
     val selectedItemName: String = "",
-    val recommendList: List<RecommendItem> = mockRecommendItemList
+    val recommendList: List<Product> = emptyList(),
 )
