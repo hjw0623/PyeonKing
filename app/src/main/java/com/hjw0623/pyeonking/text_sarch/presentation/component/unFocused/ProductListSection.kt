1868de329c09ab18acce2032ec25323600eb22e6
hjw9623 <hjw6191.wh@gmail.com>
2025-06-22 20:59:14 +0900

    feat: 초기 상품 리스트 ui 및 구성 요소 생성

---
+++ app/src/main/java/com/hjw0623/pyeonking/text_sarch/presentation/component/unFocused/ProductListSection.kt
@@ -0,0 +1,50 @@
+package com.hjw0623.pyeonking.text_sarch.presentation.component.unFocused
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.hjw0623.pyeonking.core.data.Product
+import com.hjw0623.pyeonking.core.data.mockProductList
+import com.hjw0623.pyeonking.text_sarch.data.FilterType
+import com.hjw0623.pyeonking.ui.theme.PyeonKingTheme
+
+@Composable
+fun ProductListSection(
+    selectedFilter: Map<FilterType, Boolean>,
+    products: List<Product>,
+    onProductClick: (String) -> Unit,
+    onFilterToggle: (FilterType) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier.fillMaxWidth()
+    ) {
+        CategoryBar(
+            selectedFilters = selectedFilter,
+            onFilterToggle = onFilterToggle
+        )
+        ProductList(
+            products = products,
+            onProductClick = onProductClick
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun AllProductScreenPreview() {
+    PyeonKingTheme {
+        ProductListSection(
+            selectedFilter = mapOf(
+                FilterType.ONE_PLUS_ONE to true,
+                FilterType.CU to false,
+                FilterType.GS25 to false
+            ),
+            onFilterToggle = {},
+            onProductClick = {},
+            products = mockProductList
+        )
+    }
+}
