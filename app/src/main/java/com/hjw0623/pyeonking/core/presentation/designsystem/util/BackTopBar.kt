e405d7df40a14e23dc42b4a533b1daf0edb708d4
hjw9623 <hjw6191.wh@gmail.com>
2025-06-24 02:14:19 +0900

    refactor: 공통으로 사용하기 위해 수정
    - BackTopBar 페이지 제목 항목 추가
    - PyeonKingButton modifier 추가

--- app/src/main/java/com/hjw0623/pyeonking/core/presentation/designsystem/util/BackTopBar.kt
+++ app/src/main/java/com/hjw0623/pyeonking/core/presentation/designsystem/util/BackTopBar.kt
@@ -7,28 +7,31 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.text.font.FontWeight
 
 @Composable
 fun BackBar(
     onBackClick: () -> Unit,
     title: String? = null,
+    iconTint: Color = MaterialTheme.colorScheme.primary,
+    backgroundColor: Color = MaterialTheme.colorScheme.background,
     modifier: Modifier = Modifier
 ) {
     Box(
         modifier = modifier
             .fillMaxWidth()
             .height(56.dp)
-            .background(MaterialTheme.colorScheme.background)
+            .background(backgroundColor)
     ) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
@@ -38,7 +41,7 @@ fun BackBar(
                 Icon(
                     imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                     contentDescription = null,
-                    tint = MaterialTheme.colorScheme.primary
+                    tint = iconTint
                 )
             }
         }
