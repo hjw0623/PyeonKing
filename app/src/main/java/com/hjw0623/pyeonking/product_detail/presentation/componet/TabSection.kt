8cfc59b188dc27008ebddca4ee94504fed59f839
hjw9623 <hjw6191.wh@gmail.com>
2025-06-24 02:32:39 +0900

    feat: 상품 상세 탭 전환을 위한 TabSection 컴포저블 추가

---
+++ app/src/main/java/com/hjw0623/pyeonking/product_detail/presentation/componet/TabSection.kt
@@ -0,0 +1,40 @@
+package com.hjw0623.pyeonking.product_detail.presentation.componet
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.hjw0623.pyeonking.R
+import com.hjw0623.pyeonking.product_detail.data.ProductDetailTab
+
+@Composable
+fun TabSection(
+    selectedTab: ProductDetailTab,
+    onTabClick: (ProductDetailTab) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    val tabs = listOf(ProductDetailTab.MAP, ProductDetailTab.REVIEW)
+    TabRow(
+        selectedTabIndex = tabs.indexOf(selectedTab),
+        modifier = modifier.fillMaxWidth()
+    ) {
+        tabs.forEach { tab ->
+            Tab(
+                selected = selectedTab == tab,
+                onClick = { onTabClick(tab) },
+                text = {
+                    Text(
+                        text = when (tab) {
+                            ProductDetailTab.REVIEW -> stringResource(R.string.lebel_review)
+                            ProductDetailTab.MAP -> stringResource(R.string.lebel_map)
+                        }
+                    )
+                }
+            )
+        }
+    }
+
+}
