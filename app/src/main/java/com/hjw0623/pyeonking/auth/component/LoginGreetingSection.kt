a45f6bd0be4f7d468d6e69d18982e11779e1b42a
hjw9623 <hjw6191.wh@gmail.com>
2025-06-25 10:43:06 +0900

    feat: 환영인사와 색션 LoginGreetingSection 컴포넌트 추가

---
+++ app/src/main/java/com/hjw0623/pyeonking/auth/component/LoginGreetingSection.kt
@@ -0,0 +1,48 @@
+package com.hjw0623.pyeonking.auth.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.hjw0623.pyeonking.R
+
+@Composable
+fun LoginGreetingSection(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.padding(horizontal = 16.dp),
+    ) {
+        Box(modifier = Modifier.align(Alignment.Start)) {
+            AsyncImage(
+                model = "https://robohash.org/login",
+                contentDescription = null,
+                modifier = Modifier
+                    .size(120.dp)
+                    .clip(CircleShape),
+                contentScale = ContentScale.Crop
+            )
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Text(stringResource(R.string.welcom_prompt), style = MaterialTheme.typography.titleMedium)
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(stringResource(R.string.prompt_login_for_member_service), style = MaterialTheme.typography.bodySmall)
+        Spacer(modifier = Modifier.height(24.dp))
+
+    }
+}
