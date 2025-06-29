1549c8c5311a1dd1756c9be70de38035849ff2b0
hjw9623 <hjw6191.wh@gmail.com>
2025-06-29 09:45:51 +0900

    chore: 불 필요한 파일 삭제

--- app/src/main/java/com/hjw0623/pyeonking/product_detail/presentation/ProductDetailAction.kt
+++
@@ -1,8 +0,0 @@
-package com.hjw0623.pyeonking.product_detail.presentation
-
-import com.hjw0623.pyeonking.product_detail.data.ProductDetailTab
-
-sealed interface ProductDetailAction {
-    data class OnChangeTab(val tab: ProductDetailTab) : ProductDetailAction
-    object OnWriteReviewClick : ProductDetailAction
-}
