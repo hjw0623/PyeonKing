a4a52438bbf277db6cf6a44b51f34f90417ed697
hjw9623 <hjw6191.wh@gmail.com>
2025-06-29 09:33:54 +0900

    refactor: 리뷰작성 화면 리팩토링
    -네비게이션을 위해 eventFlow 정의
    -디자인 수정

--- app/src/main/java/com/hjw0623/pyeonking/review_write/presentation/ReviewWriteScreen.kt
+++ app/src/main/java/com/hjw0623/pyeonking/review_write/presentation/ReviewWriteScreen.kt
@@ -10,18 +10,18 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -30,39 +30,56 @@ import coil.compose.AsyncImage
 import com.hjw0623.pyeonking.R
 import com.hjw0623.pyeonking.core.data.Product
 import com.hjw0623.pyeonking.core.data.mockProduct
-import com.hjw0623.pyeonking.core.presentation.designsystem.util.BackBar
 import com.hjw0623.pyeonking.core.presentation.designsystem.util.PyeonKingButton
+import com.hjw0623.pyeonking.core.presentation.designsystem.util.showToast
+import com.hjw0623.pyeonking.core.util.ObserveAsEvents
 import com.hjw0623.pyeonking.review_write.presentation.component.StarRatingSelector
 import com.hjw0623.pyeonking.review_write.presentation.component.WritingSection
 import com.hjw0623.pyeonking.ui.theme.PyeonKingTheme
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.launch
 
 @Composable
 fun ReviewWriteScreenRoot(
     modifier: Modifier = Modifier,
-    product: Product = mockProduct,
+    product: Product,
+    onReviewWriteComplete: () -> Unit
 ) {
+    val context = LocalContext.current
+    val scope = rememberCoroutineScope()
     var state by remember {
-        mutableStateOf(ReviewWriteState(product = product))
+        mutableStateOf(ReviewWriteScreenState(product = product))
+    }
+    val eventFlow = remember { MutableSharedFlow<ReviewWriteScreenEvent>() }
+
+    ObserveAsEvents(flow = eventFlow) { event ->
+        when (event) {
+            is ReviewWriteScreenEvent.Error -> {
+                showToast(context, event.error)
+            }
+
+            is ReviewWriteScreenEvent.NavigateBackToProductDetail -> {
+                onReviewWriteComplete()
+            }
+        }
     }
 
     ReviewWriteScreen(
         state = state,
         onAction = { action ->
             when (action) {
-                is ReviewWriteAction.OnContentChange -> {
+                is ReviewWriteScreenAction.OnContentChange -> {
                     state = state.copy(content = action.content)
                 }
 
-                ReviewWriteAction.OnBackClick -> {
-
-                }
-
-                is ReviewWriteAction.OnRatingChange -> {
+                is ReviewWriteScreenAction.OnRatingChange -> {
                     state = state.copy(rating = action.rating)
                 }
 
-                is ReviewWriteAction.OnSubmitClick -> {
-
+                is ReviewWriteScreenAction.OnSubmitClick -> {
+                    scope.launch {
+                        eventFlow.emit(ReviewWriteScreenEvent.NavigateBackToProductDetail)
+                    }
                 }
             }
         },
@@ -72,22 +89,13 @@ fun ReviewWriteScreenRoot(
 
 @Composable
 fun ReviewWriteScreen(
-    state: ReviewWriteState,
-    onAction: (ReviewWriteAction) -> Unit,
+    state: ReviewWriteScreenState,
+    onAction: (ReviewWriteScreenAction) -> Unit,
     modifier: Modifier = Modifier,
 ) {
 
     Column(modifier = modifier.fillMaxSize()) {
-        BackBar(
-            title = stringResource(R.string.title_write_review),
-            icon = Icons.Default.Clear,
-            onBackClick = {
-                onAction(ReviewWriteAction.OnBackClick)
-            },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 16.dp)
-        )
+
 
         Row(
             modifier = Modifier
@@ -115,14 +123,14 @@ fun ReviewWriteScreen(
                     )
                 StarRatingSelector(
                     rating = state.rating,
-                    onRatingChange = { onAction(ReviewWriteAction.OnRatingChange(it)) }
+                    onRatingChange = { onAction(ReviewWriteScreenAction.OnRatingChange(it)) }
                 )
             }
         }
 
         WritingSection(
             content = state.content,
-            onContentChange = { onAction(ReviewWriteAction.OnContentChange(it)) }
+            onContentChange = { onAction(ReviewWriteScreenAction.OnContentChange(it)) }
         )
 
         Spacer(modifier = Modifier.weight(1f))
@@ -131,7 +139,7 @@ fun ReviewWriteScreen(
             text = stringResource(R.string.action_submit),
             onClick = {
                 onAction(
-                    ReviewWriteAction.OnSubmitClick(
+                    ReviewWriteScreenAction.OnSubmitClick(
                         rating = state.rating,
                         content = state.content
                     )
@@ -151,7 +159,7 @@ fun ReviewWriteScreen(
 private fun ReviewWriteScreenPreview() {
     PyeonKingTheme {
         ReviewWriteScreen(
-            state = ReviewWriteState(product = mockProduct),
+            state = ReviewWriteScreenState(product = mockProduct),
             onAction = {},
         )
     }
