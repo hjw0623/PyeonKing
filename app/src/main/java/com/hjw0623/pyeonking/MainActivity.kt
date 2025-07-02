90e4c5704ddd964a65471d9d7c7bb01b6d0be9aa
hjw9623 <hjw6191.wh@gmail.com>
2025-07-02 13:51:23 +0900

    feat: 멀티 모듈 구조 초기화
    - core, data, presentation 모듈 생성
    - 각 모듈에 필요한 파일들 생성

--- app/src/main/java/com/hjw0623/pyeonking/MainActivity.kt
+++ app/src/main/java/com/hjw0623/pyeonking/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.hjw0623.pyeonking.core.presentation.ui.theme.PyeonKingTheme
 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
