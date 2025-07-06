d63c5d71817fbf732585bd2659e476e064d24fbd
hjw9623 <hjw6191.wh@gmail.com>
2025-07-06 22:15:54 +0900

    refactor: 키보드 옵션 파라미터 변경

--- presentation/src/main/java/com/hjw0623/presentation/screen/mypage/change_nickname/ui/ChangeNicknameScreen.kt
+++ presentation/src/main/java/com/hjw0623/presentation/screen/mypage/change_nickname/ui/ChangeNicknameScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Badge
@@ -133,7 +134,7 @@ private fun ChangeNicknameScreen(
                 } else {
                     null
                 },
-                keyboardType = KeyboardType.Text,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
                 modifier = Modifier.weight(1f)
             )
 
