248281ae601a48c02cb7d5aeff7ad909f2e12769
hjw9623 <hjw6191.wh@gmail.com>
2025-07-07 00:07:39 +0900

    refactor: 검색 결과 화면 ui 수정 및 임시 viewModel 적용

--- presentation/src/main/java/com/hjw0623/presentation/screen/search/search_result/ui/SearchResultScreen.kt
+++ presentation/src/main/java/com/hjw0623/presentation/screen/search/search_result/ui/SearchResultScreen.kt
@@ -2,38 +2,36 @@ package com.hjw0623.presentation.screen.search.search_result.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.hjw0623.core.domain.product.Product
 import com.hjw0623.core.domain.search.search_result.SearchResultNavArgs
-import com.hjw0623.core.domain.search.search_result.SearchResultSource
 import com.hjw0623.core.mockdata.mockProductList
 import com.hjw0623.core.presentation.designsystem.components.showToast
 import com.hjw0623.core.presentation.designsystem.theme.PyeonKingTheme
 import com.hjw0623.core.presentation.ui.ObserveAsEvents
-import com.hjw0623.presentation.R
+import com.hjw0623.core.presentation.ui.rememberThrottledOnClick
+import com.hjw0623.presentation.screen.factory.SearchResultViewModelFactory
 import com.hjw0623.presentation.screen.home.ui.component.ProductCardLarge
-import com.hjw0623.presentation.screen.search.search_result.SearchResultScreenAction
-import com.hjw0623.presentation.screen.search.search_result.ui.SearchResultScreenEvent
-import com.hjw0623.presentation.screen.search.search_result.SearchResultScreenState
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.launch
+import com.hjw0623.presentation.screen.search.viewmodel.SearchResultViewModel
 
 @Composable
 fun SearchResultScreenRoot(
@@ -42,19 +40,22 @@ fun SearchResultScreenRoot(
     modifier: Modifier = Modifier
 ) {
     val context = LocalContext.current
-    val scope = rememberCoroutineScope()
-    var state by remember {
-        mutableStateOf(
-            SearchResultScreenState(
-                source = navArgs.source,
-                passedQuery = navArgs.passedQuery.let { "" },
-                passedImagePath = navArgs.passedImagePath
-            )
-        )
-    }
-    val eventFlow = remember { MutableSharedFlow<SearchResultScreenEvent>() }
+    val searchResultViewModelFactory = SearchResultViewModelFactory()
+    val viewModel: SearchResultViewModel = viewModel(factory = searchResultViewModelFactory)
 
-    ObserveAsEvents(flow = eventFlow) { event ->
+    val isLoading by viewModel.isLoading.collectAsStateWithLifecycle()
+    val products by viewModel.products.collectAsStateWithLifecycle()
+    val searchTitle by viewModel.searchTitle.collectAsStateWithLifecycle()
+
+    val throttledProductClick = rememberThrottledOnClick<Product> { product ->
+        viewModel.onProductClick(product)
+    }
+    
+    LaunchedEffect(key1 = navArgs) {
+        viewModel.searchProducts(navArgs)
+    }
+
+    ObserveAsEvents(flow = viewModel.event) { event ->
         when (event) {
             is SearchResultScreenEvent.Error -> {
                 showToast(context, event.error)
@@ -67,57 +68,55 @@ fun SearchResultScreenRoot(
     }
 
     SearchResultScreen(
-        state = state,
-        onAction = { action ->
-            when (action) {
-                is SearchResultScreenAction.OnProductClick -> {
-                    scope.launch {
-                        eventFlow.emit(SearchResultScreenEvent.NavigateToProductDetail(action.product))
-                    }
-                }
-            }
-        },
-        modifier = modifier
+        modifier = modifier,
+        isLoading = isLoading,
+        products = products,
+        searchTitle = searchTitle,
+        onProductClick = throttledProductClick
     )
 }
 
 @Composable
 fun SearchResultScreen(
-    state: SearchResultScreenState,
-    onAction: (SearchResultScreenAction) -> Unit,
     modifier: Modifier = Modifier,
+    isLoading: Boolean,
+    products: List<Product>,
+    searchTitle: String,
+    onProductClick: (Product) -> Unit
 ) {
     Column(
         modifier = modifier
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.background)
     ) {
-
         Text(
-            text = when (state.source) {
-                SearchResultSource.CAMERA -> ""
-                SearchResultSource.TEXT -> stringResource(
-                    R.string.search_result_title_with_query,
-                    state.passedQuery
-                )
-            },
-            style = MaterialTheme.typography.titleMedium,
+            text = searchTitle,
+            style = MaterialTheme.typography.titleLarge,
             modifier = Modifier.padding(16.dp)
         )
 
-        LazyColumn(
-            verticalArrangement = Arrangement.spacedBy(12.dp),
-            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
-            modifier = Modifier.fillMaxSize()
-        ) {
-            items(
-                count = state.products.size,
-                key = { state.products[it].uuid }
+        if (isLoading) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
             ) {
-                ProductCardLarge(
-                    onCardClick = { onAction(SearchResultScreenAction.OnProductClick(state.products[it])) },
-                    product = state.products[it]
-                )
+                CircularProgressIndicator()
+            }
+        } else {
+            LazyColumn(
+                verticalArrangement = Arrangement.spacedBy(12.dp),
+                contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+                modifier = Modifier.fillMaxSize()
+            ) {
+                items(
+                    items = products,
+                    key = { it.uuid }
+                ) { product ->
+                    ProductCardLarge(
+                        onCardClick = { onProductClick(product) },
+                        product = product
+                    )
+                }
             }
         }
     }
@@ -128,12 +127,10 @@ fun SearchResultScreen(
 private fun SearchResultScreenPreview() {
     PyeonKingTheme {
         SearchResultScreen(
-            state = SearchResultScreenState(
-                source = SearchResultSource.TEXT,
-                products = mockProductList,
-                passedQuery = "콜라"
-            ),
-            onAction = {}
+            isLoading = false,
+            products = mockProductList,
+            searchTitle = "'콜라' 검색 결과",
+            onProductClick = {}
         )
     }
 }
