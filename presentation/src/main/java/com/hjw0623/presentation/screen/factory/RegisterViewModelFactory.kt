f05d8ec8f01dc38ac28cd5d02816bfa742885b83
hjw9623 <hjw6191.wh@gmail.com>
2025-07-15 11:16:53 +0900

    feat: AuthRepository 추가 및 onRegisterClick, onNicknameCheckClick() 추가

--- presentation/src/main/java/com/hjw0623/presentation/screen/factory/RegisterViewModelFactory.kt
+++ presentation/src/main/java/com/hjw0623/presentation/screen/factory/RegisterViewModelFactory.kt
@@ -2,16 +2,18 @@ package com.hjw0623.presentation.screen.factory
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.hjw0623.core.domain.auth.AuthRepository
 import com.hjw0623.core.domain.auth.UserDataValidator
 import com.hjw0623.presentation.screen.auth.viewmodel.RegisterViewModel
 
 class RegisterViewModelFactory(
+    private val authRepository: AuthRepository,
     private val userDataValidator: UserDataValidator
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(RegisterViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
-            return RegisterViewModel(userDataValidator) as T
+            return RegisterViewModel(authRepository, userDataValidator) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
