e9596ef458e61e1deaab622e077d3a793968198c
hjw9623 <hjw6191.wh@gmail.com>
2025-07-15 10:55:46 +0900

    feat: ksp 추가

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -27,9 +28,6 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    /*kotlinOptions {
-        jvmTarget = "17"
-    }*/
 }
 kotlin {
     jvmToolchain(17)
