d17499cd9c5797968e55727dff78051fb23cd018
hjw9623 <hjw6191.wh@gmail.com>
2025-07-17 04:55:33 +0900

    feat: changePromotionName, getFullImageUrl 추가
    - changePromotionName: 서버응답을 Ui를 위한 문자열로 변경
    - getFullImageUrl: 서버 응답에 따라 FullImageUrl 변경

--- core/src/main/java/com/hjw0623/core/presentation/ui/Formatter.kt
+++ core/src/main/java/com/hjw0623/core/presentation/ui/Formatter.kt
@@ -1,7 +1,9 @@
 package com.hjw0623.core.presentation.ui
 
 import androidx.compose.ui.graphics.Color
+import com.hjw0623.core.constants.Api.IMAGE_URL
 import com.hjw0623.core.constants.Brand
+import com.hjw0623.core.constants.Promotion
 import com.hjw0623.core.presentation.designsystem.theme.Cu
 import com.hjw0623.core.presentation.designsystem.theme.Emart24
 import com.hjw0623.core.presentation.designsystem.theme.Gs25
@@ -19,5 +21,21 @@ fun getBrandColor(
         Brand.SEVEN -> Seven
         Brand.EMART24 -> Emart24
         else -> if (isDarkTheme) errorDark else errorLight
+    }
+}
+
+fun changePromotionName(promotion: String): String {
+    return when (promotion) {
+        Promotion.ONE_PLUS_ONE_RESPONSE -> Promotion.ONE_PLUS_ONE
+        Promotion.TWO_PLUS_ONE_RESPONSE -> Promotion.TWO_PLUS_ONE
+        else -> promotion
+    }
+}
+
+fun getFullImageUrl(imgUrl: String): String {
+    return if (imgUrl.startsWith("http")) {
+        imgUrl
+    } else {
+        IMAGE_URL + imgUrl
     }
 }
