7b14565f7ba2c8eeeb7316b523f65331d9b51cb6
hjw9623 <hjw6191.wh@gmail.com>
2025-07-17 08:28:38 +0900

    feat: ReviewPage 추가

--- core/src/main/java/com/hjw0623/core/data/model/ReviewResponse.kt
+++ core/src/main/java/com/hjw0623/core/data/model/ReviewResponse.kt
@@ -16,3 +16,12 @@ data class ProductDto(
     val imageUrl: String,
     val brand: String
 )
+
+data class ReviewPage(
+    val content: List<ReviewResponse>,
+    val totalElements: Int,
+    val totalPages: Int,
+    val last: Boolean,
+    val number: Int,
+    val size: Int
+)
