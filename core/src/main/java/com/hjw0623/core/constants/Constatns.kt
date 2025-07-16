d69dddd6ac3933f638de23146a0d734da15ae9a2
hjw9623 <hjw6191.wh@gmail.com>
2025-07-17 03:19:00 +0900

    feat: 서버응답 문자열 추가

--- core/src/main/java/com/hjw0623/core/constants/Constatns.kt
+++ core/src/main/java/com/hjw0623/core/constants/Constatns.kt
@@ -39,6 +39,10 @@ object Brand {
 object Promotion {
     const val ONE_PLUS_ONE = "1+1"
     const val TWO_PLUS_ONE = "2+1"
+
+    const val ONE_PLUS_ONE_RESPONSE = "ONE_PLUS_ONE"
+
+    const val TWO_PLUS_ONE_RESPONSE = "TWO_PLUS_ONE"
 }
 
 object Api {
