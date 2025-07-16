8191faf08235c270e06155ddd2cfd02a82ff2bf6
hjw9623 <hjw6191.wh@gmail.com>
2025-07-17 04:52:02 +0900

    feat: DTO, Domain 간 매핑 로직 추가
    -ItemDto, SearchItemResponseDto

--- data/src/main/java/com/hjw0623/data/model/mapper/Mappers.kt
+++ data/src/main/java/com/hjw0623/data/model/mapper/Mappers.kt
@@ -1,9 +1,14 @@
 package com.hjw0623.data.model.mapper
 
+import com.hjw0623.core.constants.Promotion
 import com.hjw0623.core.data.model.AuthRequest
 import com.hjw0623.core.data.model.AuthResponse
+import com.hjw0623.core.data.model.Item
+import com.hjw0623.core.data.model.SearchItemResponse
 import com.hjw0623.data.model.AuthRequestDto
 import com.hjw0623.data.model.AuthResponseDto
+import com.hjw0623.data.model.ItemDto
+import com.hjw0623.data.model.SearchItemResponseDto
 
 
 fun AuthResponseDto.toDomain(): AuthResponse {
@@ -18,5 +23,29 @@ fun AuthRequest.toDto(): AuthRequestDto {
         email = this.email,
         password = this.password,
         nickname = this.nickname
+    )
+}
+
+fun ItemDto.toDomain(): Item {
+    val discountedUnitPrice = if (this.promotion == Promotion.ONE_PLUS_ONE) {
+        this.pricePerGroup / 2
+    } else {
+        this.pricePerGroup / 3
+    }
+    return Item(
+        id = this.id,
+        name = this.name,
+        imgUrl = this.imgUrl,
+        brand = this.brand,
+        promotion = this.promotion,
+        pricePerUnit = this.pricePerUnit,
+        pricePerGroup = this.pricePerGroup,
+        discountedUnitPrice = discountedUnitPrice
+    )
+}
+
+fun SearchItemResponseDto.toDomain(): SearchItemResponse {
+    return SearchItemResponse(
+        searchItems = this.searchItems.map { it.toDomain() }
     )
 }
