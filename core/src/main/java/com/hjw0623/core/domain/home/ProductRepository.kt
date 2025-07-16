1aca77ebe84f0632c09e409c6662df07e66cf526
hjw9623 <hjw6191.wh@gmail.com>
2025-07-17 04:48:47 +0900

    feat: ProductRepository 인터페이스 및 구현체 (ProductRepositoryImpl) 추가

---
+++ core/src/main/java/com/hjw0623/core/domain/home/ProductRepository.kt
@@ -0,0 +1,13 @@
+package com.hjw0623.core.domain.home
+
+import com.hjw0623.core.data.model.BaseResponse
+import com.hjw0623.core.data.model.Item
+import com.hjw0623.core.data.model.SearchItemResponse
+import com.hjw0623.core.network.DataResourceResult
+import kotlinx.coroutines.flow.Flow
+
+interface ProductRepository {
+    suspend fun getRecommendList(): Flow<DataResourceResult<BaseResponse<List<Item>>>>
+
+    suspend fun searchItems(name: String): Flow<DataResourceResult<BaseResponse<SearchItemResponse>>>
+}
