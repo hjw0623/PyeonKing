0ea82265e1a7a8c2aadb6fa9dbad9ecc71bc7440
hjw9623 <hjw6191.wh@gmail.com>
2025-07-17 04:50:10 +0900

    feat: ProductRepository 추가 및 fetchRecommendList 구현

--- presentation/src/main/java/com/hjw0623/presentation/screen/factory/HomeViewModelFactory.kt
+++ presentation/src/main/java/com/hjw0623/presentation/screen/factory/HomeViewModelFactory.kt
@@ -2,13 +2,16 @@ package com.hjw0623.presentation.screen.factory
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.hjw0623.core.domain.home.ProductRepository
 import com.hjw0623.presentation.screen.home.viewmodel.HomeViewModel
 
-class HomeViewModelFactory(): ViewModelProvider.Factory {
+class HomeViewModelFactory(
+    private val productRepository: ProductRepository
+): ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(HomeViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
-            return HomeViewModel() as T
+            return HomeViewModel(productRepository) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
