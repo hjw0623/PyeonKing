f5f4d434e6013591c3cc93685d1cb2b1f6626f6d
hjw9623 <hjw6191.wh@gmail.com>
2025-07-17 09:55:06 +0900

    feat: 매퍼 함수 추가

--- data/src/main/java/com/hjw0623/data/model/mapper/Mappers.kt
+++ data/src/main/java/com/hjw0623/data/model/mapper/Mappers.kt
@@ -3,10 +3,16 @@ package com.hjw0623.data.model.mapper
 import com.hjw0623.core.constants.Promotion
 import com.hjw0623.core.data.model.AuthRequest
 import com.hjw0623.core.data.model.AuthResponse
+import com.hjw0623.core.data.model.ChangeNicknameRequest
+import com.hjw0623.core.data.model.ChangePasswordRequest
+import com.hjw0623.core.data.model.ChangePasswordResponse
 import com.hjw0623.core.data.model.Item
 import com.hjw0623.core.data.model.SearchItemResponse
 import com.hjw0623.data.model.AuthRequestDto
 import com.hjw0623.data.model.AuthResponseDto
+import com.hjw0623.data.model.ChangeNicknameRequestDto
+import com.hjw0623.data.model.ChangePasswordRequestDto
+import com.hjw0623.data.model.ChangePasswordResponseDto
 import com.hjw0623.data.model.ItemDto
 import com.hjw0623.data.model.SearchItemResponseDto
 
@@ -48,4 +54,20 @@ fun SearchItemResponseDto.toDomain(): SearchItemResponse {
     return SearchItemResponse(
         searchItems = this.searchItems.map { it.toDomain() }
     )
+}
+
+
+fun ChangeNicknameRequest.toDto(): ChangeNicknameRequestDto {
+    return ChangeNicknameRequestDto(nickname = this.nickname)
+}
+
+fun ChangePasswordRequest.toDto(): ChangePasswordRequestDto {
+    return ChangePasswordRequestDto(
+        email = this.email,
+        password = this.password,
+        newPassword = this.newPassword
+    )
+}
+fun ChangePasswordResponseDto.toDomain(): ChangePasswordResponse {
+    return ChangePasswordResponse(result = this.result)
 }
