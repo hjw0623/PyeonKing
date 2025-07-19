b3c9deef9d879ac32047086efdd34096ea3757df
hjw9623 <hjw6191.wh@gmail.com>
2025-07-20 03:54:45 +0900

    refactor: ReviewRepository 추가 및 리뷰 불러오기 기능 추가

--- presentation/src/main/java/com/hjw0623/presentation/screen/review/viewmodel/ReviewHistoryViewModel.kt
+++ presentation/src/main/java/com/hjw0623/presentation/screen/review/viewmodel/ReviewHistoryViewModel.kt
@@ -2,25 +2,33 @@ package com.hjw0623.presentation.screen.review.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.hjw0623.core.data.model.toReviewInfo
+import com.hjw0623.core.domain.review.ReviewRepository
 import com.hjw0623.core.domain.review.review_history.ReviewInfo
-import com.hjw0623.core.util.mockdata.mockReviewHistoryList
+import com.hjw0623.core.network.DataResourceResult
 import com.hjw0623.presentation.screen.review.review_history.ui.ReviewHistoryScreenEvent
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 class ReviewHistoryViewModel(
-    // private val repository: ReviewRepository
-): ViewModel() {
+    private val reviewRepository: ReviewRepository
+) : ViewModel() {
 
     private val _isLoading = MutableStateFlow(false)
     val isLoading = _isLoading.asStateFlow()
 
     private val _reviewHistoryList = MutableStateFlow<List<ReviewInfo>>(emptyList())
     val reviewHistoryList = _reviewHistoryList.asStateFlow()
+
+    private val _currentPage = MutableStateFlow(1)
+    val currentPage = _currentPage.asStateFlow()
+
+    private val _lastPage = MutableStateFlow(1)
+    val lastPage = _lastPage.asStateFlow()
 
     private val _event = MutableSharedFlow<ReviewHistoryScreenEvent>()
     val event = _event.asSharedFlow()
@@ -29,19 +37,47 @@ class ReviewHistoryViewModel(
         fetchReviewHistory()
     }
 
-    private fun fetchReviewHistory() {
+    fun fetchReviewHistory(page: Int = 1) {
         viewModelScope.launch {
             _isLoading.value = true
-            try {
-                delay(1000)
-                _reviewHistoryList.value = mockReviewHistoryList
-            } catch (e: Exception) {
-                _event.emit(ReviewHistoryScreenEvent.Error("리뷰 내역을 불러오는 데 실패했습니다."))
-            } finally {
-                _isLoading.value = false
+            reviewRepository.getReviewByUserId(page = page).collectLatest { result ->
+                when (result) {
+                    DataResourceResult.Loading -> _isLoading.value = true
+
+                    is DataResourceResult.Success -> {
+                        _isLoading.value = false
+
+                        val newList = result.data.data.content.map { it.toReviewInfo() }
+
+                        if (page == 1) {
+                            _reviewHistoryList.value = newList
+                        } else {
+                            _reviewHistoryList.value += newList
+                        }
+
+                        _currentPage.value = page
+                        _lastPage.value = result.data.data.totalPages
+                    }
+
+                    is DataResourceResult.Failure -> {
+                        val message = result.exception.message.toString()
+                        _event.emit(ReviewHistoryScreenEvent.Error(message))
+                        _isLoading.value = false
+                    }
+
+                    else -> Unit
+                }
             }
         }
     }
+
+    fun fetchNextReviewPage() {
+        val nextPage = _currentPage.value + 1
+        if (nextPage <= _lastPage.value) {
+            fetchReviewHistory(nextPage)
+        }
+    }
+
 
     fun onEditReviewClick(reviewInfo: ReviewInfo) {
         viewModelScope.launch {
