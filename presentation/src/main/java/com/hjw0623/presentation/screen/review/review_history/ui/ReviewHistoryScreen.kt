9a9897f7280cc4232c0c02cf6e83e7e5efa4d4df
hjw9623 <hjw6191.wh@gmail.com>
2025-07-20 03:56:06 +0900

    refactor: ReviewHistoryScreenRoot에서 viewModel 외부 주입으로 변경

--- presentation/src/main/java/com/hjw0623/presentation/screen/review/review_history/ui/ReviewHistoryScreen.kt
+++ presentation/src/main/java/com/hjw0623/presentation/screen/review/review_history/ui/ReviewHistoryScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -22,34 +23,41 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.hjw0623.core.domain.review.review_history.ReviewInfo
-import com.hjw0623.core.util.mockdata.mockReviewHistoryList
+import com.hjw0623.core.presentation.designsystem.components.PyeonKingButton
 import com.hjw0623.core.presentation.designsystem.components.showToast
 import com.hjw0623.core.presentation.designsystem.theme.PyeonKingTheme
 import com.hjw0623.core.presentation.ui.ObserveAsEvents
+import com.hjw0623.core.util.mockdata.mockReviewHistoryList
 import com.hjw0623.presentation.R
-import com.hjw0623.presentation.screen.factory.ReviewHistoryViewModelFactory
 import com.hjw0623.presentation.screen.review.review_history.ui.component.ReviewHistoryListItem
 import com.hjw0623.presentation.screen.review.viewmodel.ReviewHistoryViewModel
 
 @Composable
 fun ReviewHistoryScreenRoot(
+    reviewHistoryViewModel: ReviewHistoryViewModel,
     onNavigateToReviewEdit: (ReviewInfo) -> Unit,
     modifier: Modifier = Modifier
 ) {
     val context = LocalContext.current
-    val reviewHistoryViewModelFactory = ReviewHistoryViewModelFactory()
-    val viewModel: ReviewHistoryViewModel = viewModel(factory = reviewHistoryViewModelFactory)
+    val viewModel = reviewHistoryViewModel
 
     val isLoading by viewModel.isLoading.collectAsStateWithLifecycle()
     val reviewHistoryList by viewModel.reviewHistoryList.collectAsStateWithLifecycle()
+    val currentPage by viewModel.currentPage.collectAsStateWithLifecycle()
+    val lastPage by viewModel.lastPage.collectAsStateWithLifecycle()
+
+
+    LaunchedEffect(Unit) {
+        viewModel.fetchReviewHistory(1)
+    }
 
     ObserveAsEvents(flow = viewModel.event) { event ->
         when (event) {
             is ReviewHistoryScreenEvent.Error -> {
                 showToast(context, event.error)
             }
+
             is ReviewHistoryScreenEvent.NavigateToReviewEdit -> {
                 onNavigateToReviewEdit(event.reviewInfo)
             }
@@ -60,7 +68,10 @@ fun ReviewHistoryScreenRoot(
         modifier = modifier,
         isLoading = isLoading,
         reviewHistoryList = reviewHistoryList,
-        onEditReviewClick = viewModel::onEditReviewClick
+        currentPage = currentPage,
+        lastPage = lastPage,
+        onEditReviewClick = viewModel::onEditReviewClick,
+        onLoadMoreClick = viewModel::fetchNextReviewPage
     )
 }
 
@@ -69,52 +80,73 @@ private fun ReviewHistoryScreen(
     modifier: Modifier = Modifier,
     isLoading: Boolean,
     reviewHistoryList: List<ReviewInfo>,
+    currentPage: Int,
+    lastPage: Int,
     onEditReviewClick: (ReviewInfo) -> Unit,
+    onLoadMoreClick: () -> Unit
 ) {
-    if (isLoading) {
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-            CircularProgressIndicator()
+    LazyColumn(
+        modifier = modifier
+            .padding(horizontal = 20.dp)
+            .fillMaxSize()
+    ) {
+        item {
+            Text(
+                text = stringResource(R.string.label_review_history),
+                style = MaterialTheme.typography.headlineLarge
+            )
+            Spacer(modifier = Modifier.height(40.dp))
         }
-    } else {
-        LazyColumn(
-            modifier = modifier
-                .padding(horizontal = 20.dp)
-                .fillMaxSize()
-        ) {
-            item {
-                Text(
-                    text = stringResource(R.string.label_review_history),
-                    style = MaterialTheme.typography.headlineLarge
-                )
-                Spacer(modifier = Modifier.height(40.dp))
-            }
 
-            items(
-                items = reviewHistoryList,
-                key = { it.reviewId }
-            ) { reviewItem ->
-                ReviewHistoryListItem(
-                    reviewInfo = reviewItem,
-                    onEditClick = { onEditReviewClick(reviewItem) }
+        items(
+            items = reviewHistoryList,
+            key = { it.reviewId }
+        ) { reviewItem ->
+            ReviewHistoryListItem(
+                reviewInfo = reviewItem,
+                onEditClick = { onEditReviewClick(reviewItem) }
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            if (reviewHistoryList.last() != reviewItem) {
+                Spacer(
+                    modifier = Modifier
+                        .height(1.dp)
+                        .fillMaxWidth()
+                        .background(Color.LightGray)
                 )
                 Spacer(modifier = Modifier.height(16.dp))
-                if (reviewHistoryList.last() != reviewItem) {
-                    Spacer(
-                        modifier = Modifier
-                            .height(1.dp)
-                            .fillMaxWidth()
-                            .background(Color.LightGray)
-                    )
-                    Spacer(modifier = Modifier.height(16.dp))
+            }
+        }
+
+        if (currentPage < lastPage) {
+            item {
+                Spacer(modifier = Modifier.height(20.dp))
+                PyeonKingButton(
+                    text = stringResource(
+                        R.string.action_see_more_review, currentPage, lastPage
+                    ),
+                    onClick = onLoadMoreClick,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp)
+                )
+
+                Spacer(modifier = Modifier.height(40.dp))
+            }
+        }
+
+        if (isLoading) {
+            item {
+                Box(
+                    modifier = Modifier.fillMaxWidth(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator()
                 }
             }
         }
     }
 }
-
 
 @Preview(showBackground = true)
 @Composable
@@ -123,7 +155,10 @@ private fun ReviewHistoryScreenPreview() {
         ReviewHistoryScreen(
             isLoading = false,
             reviewHistoryList = mockReviewHistoryList,
-            onEditReviewClick = {}
+            onEditReviewClick = {},
+            onLoadMoreClick = {},
+            currentPage = 1,
+            lastPage = 3
         )
     }
 }
