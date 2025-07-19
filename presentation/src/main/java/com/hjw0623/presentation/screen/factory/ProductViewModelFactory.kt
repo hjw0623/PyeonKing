cf9ed387a234fb6feec144f3680ecde8041f4a80
hjw9623 <hjw6191.wh@gmail.com>
2025-07-20 00:41:41 +0900

    feat: ProductRepository 추가 및 리뷰 불러오기 기능 추가

--- presentation/src/main/java/com/hjw0623/presentation/screen/factory/ProductViewModelFactory.kt
+++ presentation/src/main/java/com/hjw0623/presentation/screen/factory/ProductViewModelFactory.kt
@@ -2,13 +2,16 @@ package com.hjw0623.presentation.screen.factory
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.hjw0623.core.domain.product.ProductRepository
 import com.hjw0623.presentation.screen.product.viewmodel.ProductViewModel
 
-class ProductViewModelFactory() : ViewModelProvider.Factory {
+class ProductViewModelFactory(
+    private val productRepository: ProductRepository
+) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(ProductViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
-            return ProductViewModel() as T
+            return ProductViewModel(productRepository) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
