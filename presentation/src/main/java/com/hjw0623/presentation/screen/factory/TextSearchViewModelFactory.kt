d619cd4e69e4fcb63c85ddc7f7dd2dd6f3eb157b
hjw9623 <hjw6191.wh@gmail.com>
2025-07-20 11:06:53 +0900

    refactor: SearchRepository 추가 및 상품리스트 불러오기 기능 추가

--- presentation/src/main/java/com/hjw0623/presentation/screen/factory/TextSearchViewModelFactory.kt
+++ presentation/src/main/java/com/hjw0623/presentation/screen/factory/TextSearchViewModelFactory.kt
@@ -2,13 +2,16 @@ package com.hjw0623.presentation.screen.factory
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.hjw0623.core.domain.search.SearchRepository
 import com.hjw0623.presentation.screen.search.viewmodel.TextSearchViewModel
 
-class TextSearchViewModelFactory(): ViewModelProvider.Factory {
+class TextSearchViewModelFactory(
+    private val searchRepository: SearchRepository
+): ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(TextSearchViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
-            return TextSearchViewModel() as T
+            return TextSearchViewModel(searchRepository) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
