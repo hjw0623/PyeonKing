cd1f1919908f27f866931d756d827cc4f1bbf91a
hjw9623 <hjw6191.wh@gmail.com>
2025-07-23 09:41:41 +0900

    build: Java 버전 21로 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     buildFeatures {
         compose = true
@@ -43,7 +43,7 @@ android {
     }
 }
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(21)
 }
 dependencies {
     implementation(project(":core"))
