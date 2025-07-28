322eb826a58c1a1ba87b92d40a3258018ae138a1
hjw9623 <hjw6191.wh@gmail.com>
2025-07-28 12:47:58 +0900

    feat: 앱 전역 Application 클래스 정의 및 생성

---
+++ app/src/main/java/com/hjw0623/pyeonking/PyeonKingApplication.kt
@@ -0,0 +1,45 @@
+package com.hjw0623.pyeonking
+
+import android.app.Application
+import coil3.ImageLoader
+import coil3.PlatformContext
+import coil3.SingletonImageLoader
+import coil3.disk.DiskCache
+import coil3.disk.directory
+import coil3.memory.MemoryCache
+import com.hjw0623.core.data.UserPreferenceDataStore
+import com.hjw0623.core.domain.AuthManager
+import timber.log.Timber
+
+const val COIL_MEMORY_CACHE_SIZE_PERCENT = 0.1
+const val COIL_DISK_CACHE_DIR_NAME = "coil_file_cache"
+const val COIL_DISK_CACHE_MAX_SIZE = 1024 * 1024 * 30
+
+class PyeonKingApplication: Application(), SingletonImageLoader.Factory {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+        pyeonKingApplication = this
+
+        val userPrefs = UserPreferenceDataStore(applicationContext)
+        AuthManager.initialize(userPrefs)
+
+    }
+    companion object {
+        private lateinit var pyeonKingApplication: PyeonKingApplication
+        fun getPyeonKingApplication() = pyeonKingApplication
+    }
+    override fun newImageLoader(context: PlatformContext): ImageLoader = ImageLoader.Builder(context)
+        .memoryCache {
+            MemoryCache.Builder()
+                .maxSizePercent(context, COIL_MEMORY_CACHE_SIZE_PERCENT)
+                .build()
+        }
+        .diskCache {
+            DiskCache.Builder()
+                .directory(filesDir.resolve(COIL_DISK_CACHE_DIR_NAME))
+                .maximumMaxSizeBytes(COIL_DISK_CACHE_MAX_SIZE.toLong())
+                .build()
+        }.build()
+}
+
