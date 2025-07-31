77377397b79238d3115cbbaaba30eb1760b95d5f
hjw9623 <hjw6191.wh@gmail.com>
2025-07-31 23:28:46 +0900

    feat: SplashScreen 적용

--- app/src/main/java/com/hjw0623/pyeonking/MainActivity.kt
+++ app/src/main/java/com/hjw0623/pyeonking/MainActivity.kt
@@ -4,7 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import com.hjw0623.core.presentation.designsystem.theme.PyeonKingTheme
+import com.hjw0623.presentation.screen.splash.SplashScreen
+import kotlinx.coroutines.delay
 
 
 class MainActivity : ComponentActivity() {
@@ -13,7 +20,18 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PyeonKingTheme {
-                MainScreen()
+                var isSplashShown by remember { mutableStateOf(true) }
+
+                LaunchedEffect(Unit) {
+                    delay(2000L)
+                    isSplashShown = false
+                }
+
+                if (isSplashShown) {
+                    SplashScreen()
+                } else {
+                    MainScreen()
+                }
             }
         }
     }
