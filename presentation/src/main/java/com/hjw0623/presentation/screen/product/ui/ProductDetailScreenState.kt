70948871f58bca62af11d64a31df2d27e9d4fc9a
hjw9623 <hjw6191.wh@gmail.com>
2025-08-17 03:23:16 +0900

    refactor: ProductDetailScreen 상태값 ProductDetailScreenState로 통합

---
+++ presentation/src/main/java/com/hjw0623/presentation/screen/product/ui/ProductDetailScreenState.kt
@@ -0,0 +1,18 @@
+package com.hjw0623.presentation.screen.product.ui
+
+import com.hjw0623.core.business_logic.model.product.Product
+import com.hjw0623.core.business_logic.model.product.ProductDetailTab
+import com.hjw0623.core.business_logic.model.product.ReviewItem
+
+data class ProductDetailScreenState(
+    val isSummaryLoading: Boolean = false,
+    val isReviewLoading: Boolean = false,
+    val product: Product? = null,
+    val selectedTab: ProductDetailTab = ProductDetailTab.REVIEW,
+    val reviewList: List<ReviewItem> = emptyList(),
+    val ratingList: List<Int> = List(5) { 0 },
+    val avgRating: Double = 0.0,
+    val reviewSum: Int = 0,
+    val currentPage: Int = 1,
+    val lastPage: Int = 0
+)
