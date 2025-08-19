d076dd451c807faab9f337f512355cd8f649fadb
hjw9623 <hjw6191.wh@gmail.com>
2025-08-19 16:27:10 +0900

    refactor: @HiltAndroidApp 어노테이션 추가

--- app/src/main/java/com/hjw0623/pyeonking/PyeonKingApplication.kt
+++ app/src/main/java/com/hjw0623/pyeonking/PyeonKingApplication.kt
@@ -7,40 +7,39 @@ import coil3.SingletonImageLoader
 import coil3.disk.DiskCache
 import coil3.disk.directory
 import coil3.memory.MemoryCache
-import com.hjw0623.core.business_logic.auth.AuthManager
-import com.hjw0623.core.business_logic.repository.UserDataStoreRepository
-import com.hjw0623.data.repository.UserDataStoreRepositoryImpl
+import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
 const val COIL_MEMORY_CACHE_SIZE_PERCENT = 0.25
 const val COIL_DISK_CACHE_DIR_NAME = "coil_file_cache"
 const val COIL_DISK_CACHE_MAX_SIZE = 1024 * 1024 * 50
 
-class PyeonKingApplication: Application(), SingletonImageLoader.Factory {
+@HiltAndroidApp
+class PyeonKingApplication : Application(), SingletonImageLoader.Factory {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
         pyeonKingApplication = this
 
-        val userDataStoreRepository: UserDataStoreRepository = UserDataStoreRepositoryImpl(applicationContext)
-        AuthManager.initialize(userDataStoreRepository)
-
     }
+
     companion object {
         private lateinit var pyeonKingApplication: PyeonKingApplication
         fun getPyeonKingApplication() = pyeonKingApplication
     }
-    override fun newImageLoader(context: PlatformContext): ImageLoader = ImageLoader.Builder(context)
-        .memoryCache {
-            MemoryCache.Builder()
-                .maxSizePercent(context, COIL_MEMORY_CACHE_SIZE_PERCENT)
-                .build()
-        }
-        .diskCache {
-            DiskCache.Builder()
-                .directory(filesDir.resolve(COIL_DISK_CACHE_DIR_NAME))
-                .maximumMaxSizeBytes(COIL_DISK_CACHE_MAX_SIZE.toLong())
-                .build()
-        }.build()
+
+    override fun newImageLoader(context: PlatformContext): ImageLoader =
+        ImageLoader.Builder(context)
+            .memoryCache {
+                MemoryCache.Builder()
+                    .maxSizePercent(context, COIL_MEMORY_CACHE_SIZE_PERCENT)
+                    .build()
+            }
+            .diskCache {
+                DiskCache.Builder()
+                    .directory(filesDir.resolve(COIL_DISK_CACHE_DIR_NAME))
+                    .maximumMaxSizeBytes(COIL_DISK_CACHE_MAX_SIZE.toLong())
+                    .build()
+            }.build()
 }
 
