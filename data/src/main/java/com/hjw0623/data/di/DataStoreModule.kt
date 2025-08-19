6dcaac95a893867f04e9fc2fe2b344adfbf68f5d
hjw9623 <hjw6191.wh@gmail.com>
2025-08-19 16:01:28 +0900

    feat: DataStore hilt Di 모듈 추가

---
+++ data/src/main/java/com/hjw0623/data/di/DataStoreModule.kt
@@ -0,0 +1,25 @@
+package com.hjw0623.data.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStoreFile
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataStoreModule {
+    @Provides
+    @Singleton
+    fun providePreferenceDataStore(
+        @ApplicationContext context: Context
+    ): DataStore<Preferences> = PreferenceDataStoreFactory.create(
+        produceFile = {context.preferencesDataStoreFile("user_prefs")}
+    )
+}
