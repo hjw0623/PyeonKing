ebae24228c905caa8b92bd91a8547bc064db86bd
hjw9623 <hjw6191.wh@gmail.com>
2025-08-19 16:04:02 +0900

    chore: import문 정리

--- core/src/main/java/com/hjw0623/core/presentation/ui/ShowBottomBar.kt
+++ core/src/main/java/com/hjw0623/core/presentation/ui/ShowBottomBar.kt
@@ -1,7 +1,5 @@
 package com.hjw0623.core.presentation.ui
 
-import com.hjw0623.core.constants.ScreenRoutes
-
 fun shouldShowBottomBar(route: String?): Boolean {
     val simpleName = route?.substringAfterLast(".") ?: return false
     return simpleName in listOf("Home", "Camera", "TextSearch", "MyPage")
