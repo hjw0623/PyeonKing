097fb7137f27220c5bc1d0c8a334452818ecdc66
hjw9623 <hjw6191.wh@gmail.com>
2025-08-26 16:19:01 +0900

    feat: TokenProvider 추가

---
+++ data/src/main/java/com/hjw0623/data/di/TokenProvider.kt
@@ -0,0 +1,44 @@
+package com.hjw0623.data.di
+
+import com.hjw0623.core.business_logic.repository.UserDataStoreRepository
+import com.hjw0623.core.di.ApplicationScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class TokenProvider @Inject constructor(
+    userDataStore: UserDataStoreRepository,
+    @ApplicationScope private val applicationScope: CoroutineScope
+) {
+    val tokenFlow: StateFlow<String?> =
+        userDataStore.accessTokenFlow.stateIn(
+            scope = applicationScope,
+            started = SharingStarted.Eagerly,
+            initialValue = null
+        )
+
+    fun getToken(): String? = tokenFlow.value
+}
+
+/*
+    @Singleton
+    class TokenProvider @Inject constructor(
+        private val userDataStore: UserDataStoreRepository
+    ) {
+        @Volatile private var cachedToken: String? = null
+
+        init {
+            CoroutineScope(Dispatchers.IO).launch {
+                userDataStore.accessTokenFlow.collectLatest { token ->
+                    cachedToken = token
+                }
+            }
+        }
+
+        fun getToken(): String? = cachedToken
+    }
+ */
