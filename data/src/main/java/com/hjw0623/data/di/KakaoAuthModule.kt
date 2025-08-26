f98b915692fb2781dfd06c2db90f992660cd2117
hjw9623 <hjw6191.wh@gmail.com>
2025-08-26 16:09:28 +0900

    refactor: Qualifier 이동

--- data/src/main/java/com/hjw0623/data/di/KakaoAuthModule.kt
+++ data/src/main/java/com/hjw0623/data/di/KakaoAuthModule.kt
@@ -5,19 +5,14 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Qualifier
 import javax.inject.Singleton
-
-@Qualifier
-@Retention(AnnotationRetention.BINARY)
-annotation class KakaoAuthHeader
 
 @Module
 @InstallIn(SingletonComponent::class)
 object KakaoAuthModule {
+
     @Provides
     @Singleton
     @KakaoAuthHeader
-    fun provideKakaoAuthHeader(): String =
-        BuildConfig.KAKAO_API_KEY
+    fun provideKakaoAuthHeader(): String = BuildConfig.KAKAO_API_KEY
 }
