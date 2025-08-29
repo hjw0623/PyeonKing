16290d0b0a29a45b864a0c57a8290ad268c9a6b6
hjw9623 <hjw6191.wh@gmail.com>
2025-08-29 23:41:54 +0900

    chore: import문 추가

--- core/src/main/java/com/hjw0623/core/android/di/LocationModule.kt
+++ core/src/main/java/com/hjw0623/core/android/di/LocationModule.kt
@@ -1,5 +1,15 @@
 package com.hjw0623.core.android.di
 
+import android.content.Context
+import com.hjw0623.core.android.location.AndroidLocationObserver
+import com.hjw0623.core.domain.location.LocationObserver
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
 @Module
 @InstallIn(SingletonComponent::class)
 object LocationModule {
